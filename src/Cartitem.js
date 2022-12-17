31d4768336f872ad8aa842eabd15eb3dc760d3dc
Devil200207 <74138871+Devil200207@users.noreply.github.com>
2022-12-17 19:07:07 +0530

    added the increacing and decreasing button function

--- src/Cartitem.js
+++ src/Cartitem.js
@@ -14,7 +14,20 @@ class CartItem extends React.Component
     }
     increaseQuantity =  () =>
     {
-        
+        this.setState({
+            qty:this.state.qty+1
+        });
+    }
+    decreaseQuantity = () =>
+    {
+        const {qty} = this.state;
+        if(qty == 0)
+        {
+            return;
+        }
+        this.setState({
+            qty: this.state.qty-1
+        });
     }
     render()
     {
@@ -30,7 +43,7 @@ class CartItem extends React.Component
                     <div style={{color:'#777'}}>Qty: {qty}</div>
                     <div className="cart-item-actions">
                         <img alt="increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/1828/1828926.png" onClick={this.increaseQuantity}/>
-                        <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/992/992683.png"/>
+                        <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/992/992683.png" onClick={this.decreaseQuantity}/>
                         <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/484/484662.png"/>
                     </div>
                 </div>
